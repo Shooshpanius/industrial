01ea633a25926ba3f6e987a435dd8dc3066bf77a
Shooshpanius <admin@in-da-house.ru>
2015-01-28 17:26:37 +0300

    Test

--- app/controllers/machine/pfm1_controller.rb
+++ app/controllers/machine/pfm1_controller.rb
@@ -1,10 +1,16 @@
 class Machine::Pfm1Controller < ApplicationController
+
+
+
+
   def index
   end
 
 
 
   def srv_from_plu
+
+    protect_from_forgery with: :null_session
 
     render nothing: true
 
